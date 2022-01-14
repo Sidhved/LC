bb1df064dcf892b4204afa085f29fe15f0be50c8
Sidhved <sidhved.warik@gmail.com>
2022-01-14 09:55:15 +0530

    Day 14

---
+++ Python/98 - Validate Binary Search Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+from async_timeout import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.isValidBSTRecu(root, float("-inf"), float("inf"))
+    
+    def isValidBSTRecu(self, root, low, high):
+        if root is None:
+            return True
+        
+        return low < root.val and root.val < high and self.isValidBSTRecu(root.left, low, root.val) and self.isValidBSTRecu(root.right, root.val, high)
