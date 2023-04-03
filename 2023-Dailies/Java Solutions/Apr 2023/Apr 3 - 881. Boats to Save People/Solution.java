b5863defd8b309f2f502cd4920f85e70cf2874cc
Sidhved <sidhved.warik@gmail.com>
2023-04-03 12:17:20 +0530

    Apr 3

---
+++ 2023-Dailies/Java Solutions/Apr 2023/Apr 3 - 881. Boats to Save People/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int ans = 0; 
+        for (int hi = people.length - 1, lo = 0; hi >= lo; --hi, ++ans) { // high end always moves
+            if (people[lo] + people[hi] <= limit) { ++lo; } // low end moves only if it can fit in a boat with high end.
+        }
+        return ans;
+    }
+}
--- 2023-Dailies/README.md
+++ 2023-Dailies/README.md
@@ -97,4 +97,5 @@
 | ----- | -------- |
 | Apr 1 | [704. Binary Search](https://leetcode.com/problems/binary-search/) |
 | Apr 2 | [2300. Successful Pairs of Spells and Potions](https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/) |
+| Apr 3 | [881. Boats to Save People](https://leetcode.com/problems/boats-to-save-people/description/) |
 | ----- | -------- |
